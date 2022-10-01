cba621313a590d778fc1860f1fd5cc45a8b29bae
Gustavo <107156324+GustavoPucienik@users.noreply.github.com>
2022-10-01 14:26:22 -0300

    Update portifoliopython.py

--- portifoliopython.py
+++ portifoliopython.py
@@ -2,4 +2,10 @@ import math as m
 peso = float(input('Qual é o seu peso ?'))
 altura = float(input('qual é a sua altura?'))
 imc = peso / (altura ** 2)
-print(f'O seu IMC é {imc}')
+if imc < 18.5:
+    print('Você esta abaixo do peso.')
+elif 18.5 <= imc < 25:
+    print('Você esta no peso ideal.')
+elif 25 <= imc:
+    print('Você esta em sobrepeso.')
+print('O seu IMC é {:.2f}'.format(imc))
